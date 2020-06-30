57511cf6a368f5333711138e2a8423023d96df50
Charles-Fieseler <charles.fieseler@gmail.com>
2020-06-29 19:06:33 -0700

    Refactored to work with multiple files

--- examples/WBFM_fromCBE/test_bcpd_package.py
+++ examples/WBFM_fromCBE/test_bcpd_package.py
@@ -9,6 +9,8 @@ import os
 # My imports
 import pandas as pd
 from sklearn.neighbors import NearestNeighbors
+import glob
+import re
 
 ##
 ## Helper functions
@@ -42,7 +44,6 @@ def prepare_source_and_target_nonrigid_3d(source_filename,
     print(target)
     return source, target
 
-
 def correspondence_from_transform(tf_param, source, target):
     """
     From the learned registration and the source and target distributions,
@@ -63,7 +64,6 @@ def correspondence_from_transform(tf_param, source, target):
 
     return indices
 
-
 def save_indices(indices, fname=None):
     """ Saves indices (csv) using a standard format """
 
@@ -73,56 +73,63 @@ def save_indices(indices, fname=None):
         fname = 'test_bcpd_indices.csv'
     df.to_csv(fname, header=False)
 
-import glob
-import os
+    return fname
 
 def save_indices_DLC(indices, centroid_fnames,
                      fname=None,
                      scorer='Charlie'):
 
-    num_neurons = np.shape(indices)[0]
+    num_neurons, num_tracked_pairs = np.shape(indices)
 
     # Original centroid statistics have the XYZ locations
-    centroid_dfs = [pd.read_csv(fname) for fname in centroid_fnames]
+    centroid_dfs = []
+    centroid_nums = []
+    for fname in centroid_fnames:
+        # Get the number in the filename
+        this_num = re.findall(r'\d+', fname)
+        centroid_nums.append(int(this_num[0]))
+        centroid_dfs.append(pd.read_csv(fname))
+    print(centroid_nums)
+    # print(type(centroid_nums[0]))
     dataFrame = None
     coords = np.empty((len(centroid_dfs),3,))
 
     output_path='.'
-
     # Get list of images
     #  Instead of looking at all image files, only parse the processed centroids
-    imlist=[]
-    imlist.extend([fn for fn in glob.glob(os.path.join(output_path,'*.csv')) if ('Statistics' in fn)])
-    # imtype = '*.tif'
-    # imlist.extend([fn for fn in glob.glob(os.path.join(output_path,imtype)) if ('labeled.png' not in fn)])
+    # imlist=[]
+    # imlist.extend([fn for fn in glob.glob(os.path.join(output_path,'*.csv')) if ('Statistics' in fn)])
 
-    if len(imlist)==0:
+    if len(centroid_fnames)==0:
         print("No images found; aborting")
         return
     else:
-        print("{} images found".format(len(imlist)), imlist)
+        print("{} images found".format(len(centroid_fnames)), centroid_fnames)
 
-    index = np.sort(imlist)
+    # index = np.sort(imlist)
     # print(index)
     print('Working on folder: {}'.format(os.path.split(str(output_path))[-1]))
     print("Note: this does not have the exact DLC format, but is specific to Linux")
     # Note: only works for single-digit indexed images
 
     # Define output for DLC on cluster
-    # subfoldername = 'mCherry_T000001.ome'# TODO
     subfoldername = 'test_100frames.ome'# TODO
     # TODO: hardcode linux filesep
-    relativeimagenames=['/'.join(('labeled-data',subfoldername,'img{}.tif'.format(n+1))) for n in range(len(index))]
-    # relativeimagenames=[os.path.join('labeled-data',subfoldername,'mCherry_T00000{}.ome.tiff'.format(n)) for n in range(len(index))]
+    # Use the numbers from the centroid files
+    print(centroid_nums)
+    relativeimagenames=['/'.join(('labeled-data',subfoldername,'img{}.tif'.format(i))) for i in centroid_nums]
+    print(relativeimagenames)
 
     # Build correctly DLC-formatted dataframe
-    for i in range(num_neurons):
-        bodypart = 'neuron{}'.format(i)
+    for i_neuron in range(num_neurons):
+        bodypart = 'neuron{}'.format(i_neuron)
 
-        # Get the index of the neuron in each file
-        #   TODO: multiple files
-        ind_in_files = [i, indices[i][0]]
-        print("Tracked neuron from {} (source) to {} (target)".format(ind_in_files[0], ind_in_files[1]))
+        # Get the index of THIS neuron in each file
+        target_ind = [indices[i_neuron, i_target] for i_target in range(num_tracked_pairs)]
+        ind_in_files = [i_neuron]
+        ind_in_files.extend(target_ind)
+        # ind_in_files = [i_neuron, indices[i_neuron][0]]
+        # print("Tracked neuron from {} (source) to {} (target)".format(ind_in_files[0], ind_in_files[1]))
 
         # Get xyz coordinates for one neuron, for all files
         # print(centroid_dfs[0]['X'])
@@ -141,35 +148,54 @@ def save_indices_DLC(indices, centroid_fnames,
     dataFrame.to_csv(os.path.join(output_path,"CollectedData_" + scorer + ".csv"))
     dataFrame.to_hdf(os.path.join(output_path,"CollectedData_" + scorer + '.h5'),'df_with_missing',format='table', mode='w')
 
-
-
-
-
 ##
 ## Initialize and align
 ##
-# template_fname = 'Statistics for mCherry_T00000%d.ome - Denoised - Hessian - Extended Minima.csv'
-# template_fname = os.path.join('img%d_analysis','Statistics for img.csv')
-template_fname = 'Statistics for img%d.csv'
-target_fname = template_fname % 2
-source_fname = template_fname % 1
 
-source, target = prepare_source_and_target_nonrigid_3d(source_fname,
-                                                       target_fname,
-                                                       0.005)
-cbs = [callbacks.Open3dVisualizerCallback(source, target)]
-# cbs = []
+if __name__ == "__main__":
+    # Visualization
+    to_plot = False
 
-##
-## Do BCPD and visualize
-##
-tf_param = bcpd.registration_bcpd(source, target, w=1e-12,
-                                  #gamma=10.0, #lmd=0.2, #k = 1e2,
-                                  maxiter=100,
-                                  callbacks=cbs)
-# Also print
-indices = correspondence_from_transform(tf_param, source, target)
-save_indices(indices)
+    # Get list of processed frames in folder
+    output_path='.'
+    # target_fnames=[]
+    target_fnames = [fn for fn in glob.glob(os.path.join(output_path,'*.csv')) if ('Statistics' in fn)]
 
-## directly output in DLC format
-save_indices_DLC(indices, [source_fname, target_fname])
+    # Choose a source frame; the rest are targets
+    source_index = 3
+    source_fname = target_fnames.pop(source_index)
+    print("Source filename: ", source_fname)
+    print("Target fnames: ", target_fnames)
+
+    # Loop over the pairs
+    all_indices = None
+    for t_fname in target_fnames:
+        # All the same source
+        source, target = prepare_source_and_target_nonrigid_3d(source_fname,
+                                                               t_fname,
+                                                               0.005)
+        if to_plot:
+            cbs = [callbacks.Open3dVisualizerCallback(source, target)]
+        else:
+            cbs = []
+        # Do BCPD
+        print("Note: the value of w may need to be tweaked")
+        tf_param = bcpd.registration_bcpd(source, target, w=1e-12,
+                                          #gamma=10.0, #lmd=0.2, #k = 1e2,
+                                          maxiter=100,
+                                          callbacks=cbs)
+        # Compute correspondence
+        indices = correspondence_from_transform(tf_param, source, target)
+        # save_indices(indices)
+        if all_indices is None:
+            all_indices = indices
+        else:
+            # print(all_indices.shape)
+            # print(indices.shape)
+            np.hstack((all_indices, indices))
+
+
+    ## directly output in DLC format
+    all_fnames = [source_fname].extend(target_fnames)
+    print(all_fnames)
+    save_indices_DLC(indices, all_fnames)
