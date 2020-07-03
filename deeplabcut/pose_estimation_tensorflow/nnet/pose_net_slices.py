6b593514c36d0ad07597ea570311dffe1398c670
Charles-Fieseler <charles.fieseler@gmail.com>
2020-07-03 16:24:07 -0700

    Fix expansion dims

--- deeplabcut/pose_estimation_tensorflow/nnet/pose_net_slices.py
+++ deeplabcut/pose_estimation_tensorflow/nnet/pose_net_slices.py
@@ -66,16 +66,16 @@ def expand_depth(end_points4d, block_size):
     # tf.print("Shape of end_points4d: ", tf.shape(end_points4d))
     # tf.print("Block size: ", block_size)
     # map_fn needs channels to be first; currently 3
-    # space_to_depth needs depth to be last; treat batch as depth
+    # space_to_depth needs depth to be last; this will be expanded
     print("BATCH MUST BE 1")
-    end_points4d_ch_first = tf.transpose(end_points4d, perm=[3,1,2,0])
+    end_points4d_ch_first = tf.expand_dims(
+                            tf.transpose(end_points4d, perm=[3,0,1,2]),
+                            axis=-1)
     end_points5d_ch_first = tf.map_fn(lambda x: tf.space_to_depth(x,block_size),
                                       end_points4d_ch_first)
     # Output should be: (batch, X, Y, Z, joints)
-    end_points5d = tf.expand_dims(
-                    tf.transpose(end_points5d_ch_first, perm=[1,2,3,0]),
-                    axis=0 # i.e. the batch dimension
-                    )
+    #   Only need to move channels back to the end
+    end_points5d = tf.transpose(end_points5d_ch_first, perm=[1,2,3,4,0])
     # end_points5d = tf.reshape(end_points4d, shape_5d)
 
     return end_points5d
